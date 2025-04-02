fd372ef8855d24fdf81d672593ad3c901d91e618
Christin Mikolai <mail@christinmikolai.de>
2025-04-02 23:36:36 +0200

    refactor: Move book code to a new file

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,9 +1,20 @@
+import { Book } from './Book';
+
 export const BookShelf = ({ status }) => {
 
     return (
         <div>
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{status}</h2>
+
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+                            <Book />
+                        </li>
+                    </ol>
+                </div>
+                
             </div>
             {/* <div className="bookshelf">
                 <h2 className="bookshelf-title">Currently Reading</h2>
